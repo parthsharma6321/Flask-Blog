ca73cf9ba025f82451999f5add03c8b5206aea55
parthsharma6321 <parthsharma6321@gmail.com>
2023-05-05 03:55:05 +0530

    modified:   flaskblog/config.py

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,10 @@
 import os
-# postgresql://flask_blog_database_5o4g_user:nAIRRbaApebexSUInLoRJWTxNWuIIo1E@dpg-cha296m7avj5o4b0vm5g-a.singapore-postgres.render.com/flask_blog_database_5o4g
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI_2')
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = os.environ.get('MAIL_PORT')
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
